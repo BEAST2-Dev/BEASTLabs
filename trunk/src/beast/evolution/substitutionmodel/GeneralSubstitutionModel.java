76af4adff16bf07f4086c378846e663485ba0651
higgs.ml <higgs.ml@a0f7aa36-7498-2ff5-2863-09083c5c2a5c>
2010-09-07 23:10:56 +0000

    start getting general substitution model to work

--- trunk/src/beast/evolution/substitutionmodel/GeneralSubstitutionModel.java
+++ trunk/src/beast/evolution/substitutionmodel/GeneralSubstitutionModel.java
@@ -28,27 +28,38 @@ package beast.evolution.substitutionmodel;
 
 import beast.core.Description;
 import beast.core.Input;
+import beast.core.Valuable;
 import beast.core.Input.Validate;
-import beast.core.parameter.RealParameter;
 
 @Description("Specifies transition probability matrix with no restrictions on the rates other " +
         "than that one of the is equal to one and the others are specified relative to " +
         "this unit rate. Works for any number of states.")
 public class GeneralSubstitutionModel extends SubstitutionModel.Base {
-    public Input<RealParameter> rates =
-            new Input<RealParameter>("rates", "rate parameter which defines the transition rate matrix", Validate.REQUIRED);
+    public Input<Valuable> m_rates =
+            new Input<Valuable>("rates", "Rate parameter which defines the transition rate matrix. " +
+            		"Only the off-diagonal entries need to be specified (diagonal makes row sum to zero in a " +
+            		"rate matrix). Entry i specifies the rate of from i%n to floor(i/(n-1)) where " +
+            		"n is the number of states.", Validate.REQUIRED);
 
+    int m_nStates;
+    double [][] m_rateMatrix;
+    
     @Override
     public void initAndValidate() throws Exception {
         updateMatrix = true;
-
-        eigenSystem = getDefaultEigenSystem(frequencies.get().getFreqs().length);
+        m_nStates = frequencies.get().getFreqs().length;
+        if (m_rates.get().getDimension() != m_nStates * (m_nStates-1)) {
+        	throw new Exception("Dimension of input 'rates' is " + m_rates.get().getDimension() + " but a " +
+        			"rate matrix of dimension " + m_nStates + "x" + (m_nStates -1) + "=" + m_nStates * (m_nStates -1) + " was " +
+        			"expected");
+        }
+        eigenSystem = new DefaultEigenSystem(m_nStates);
+        m_rateMatrix = new double[m_nStates][m_nStates];
     } // initAndValidate
 
     private double[] relativeRates;
     private double[] storedRelativeRates;
 
-    @SuppressWarnings("unused")
 	private EigenSystem eigenSystem;
     
     private EigenDecomposition eigenDecomposition;
@@ -59,15 +70,70 @@ public class GeneralSubstitutionModel extends SubstitutionModel.Base {
 
     @Override
     public void getTransitionProbabilities(double distance, double[] matrix) {
-    	// TODO: implement
+        int i, j, k;
+        double temp;
+
+        // this must be synchronized to avoid being called simultaneously by
+        // two different likelihood threads - AJD
+        synchronized (this) {
+            if (updateMatrix) {
+            	setupRateMatrix();
+            	eigenDecomposition = eigenSystem.decomposeMatrix(m_rateMatrix);
+            	updateMatrix = false;
+            }
+        }
+
+        // implemented a pool of iexp matrices to support multiple threads
+        // without creating a new matrix each call. - AJD
+        double[] iexp = new double[m_nStates * m_nStates];
+        // Eigen vectors
+        double[] Evec = eigenDecomposition.getEigenVectors();
+        // inverse Eigen vectors
+        double[] Ievc = eigenDecomposition.getInverseEigenVectors();
+        // Eigen values
+        double[] Eval = eigenDecomposition.getEigenValues();
+        System.arraycopy(Ievc, 0, iexp, 0, m_nStates * m_nStates);//popiexp();
+        for (i = 0; i < m_nStates; i++) {
+            temp = Math.exp(distance * Eval[i]);
+            for (j = 0; j < m_nStates; j++) {
+                iexp[i * m_nStates + j] = Ievc[i * m_nStates + j] * temp;
+            }
+        }
+
+        int u = 0;
+        for (i = 0; i < m_nStates; i++) {
+            for (j = 0; j < m_nStates; j++) {
+                temp = 0.0;
+                for (k = 0; k < m_nStates; k++) {
+                    temp += Evec[i * m_nStates + k] * iexp[k * m_nStates + j];
+                }
+
+                matrix[u] = Math.abs(temp);
+                u++;
+            }
+        }
     }
 
+    /** sets up rate matrix **/
+    public void setupRateMatrix() {
+    	Valuable rates = m_rates.get();
+	    for (int i = 0; i < m_nStates; i++) {
+	    	m_rateMatrix[i][i] = 0;
+		    for (int j = 0; j < i; j++) {
+		    	m_rateMatrix[i][j] = rates.getArrayValue(i * (m_nStates -1) + j);
+		    	m_rateMatrix[i][i] -= rates.getArrayValue(i *(m_nStates -1) + j);
+		    }
+		    for (int j = i+1; j < m_nStates; j++) {
+		    	m_rateMatrix[i][j] = rates.getArrayValue(i * (m_nStates -1) + j-1);
+		    	m_rateMatrix[i][i] -= rates.getArrayValue(i *(m_nStates -1) + j-1);
+		    }
+	    }
+	} // setupRelativeRates
+
+    
+    @Override
     public EigenDecomposition getEigenDecomposition() {
         return null;
-    }
-
-    protected EigenSystem getDefaultEigenSystem(int stateCount) {
-        return new DefaultEigenSystem(stateCount);
     }
 
     @Override
@@ -101,4 +167,5 @@ public class GeneralSubstitutionModel extends SubstitutionModel.Base {
         updateMatrix = true;
     	return true;
     }
-}
+    
+} // class GeneralSubstitutionModel
