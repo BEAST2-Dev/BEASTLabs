45e7a76a293da4978782472288a521b96d87d741
higgs.ml <higgs.ml@a0f7aa36-7498-2ff5-2863-09083c5c2a5c>
2011-03-21 19:44:50 +0000

    allow multiple sets for monophyletic constraints

--- src/beast/evolution/tree/MonophyleticConstraint.java
+++ src/beast/evolution/tree/MonophyleticConstraint.java
@@ -20,51 +20,62 @@ import beast.evolution.alignment.TaxonSet;
 		"no other taxa have. This can be used as part of the prior.")
 public class MonophyleticConstraint extends Distribution {
 	public Input<Tree> m_tree = new Input<Tree>("tree","tree to apply the constraint on", Validate.REQUIRED);
-	public Input<TaxonSet> m_set = new Input<TaxonSet>("set","set taxa that should be monophyletic", Validate.REQUIRED);
+	public Input<List<TaxonSet>> m_set = new Input<List<TaxonSet>>("set","set taxa that should be monophyletic", new ArrayList<TaxonSet>(), Validate.REQUIRED);
 	public Input<Alignment> m_taxa = new Input<Alignment>("taxa","set taxa to choose from", Validate.REQUIRED);
 
 	
-	List<Integer> m_nIDs;
+	List<Integer>[] m_nIDs;
 	
+	@SuppressWarnings("unchecked")
 	@Override
 	public void initAndValidate() throws Exception {
-		List<Taxon> set = m_set.get().m_taxonset.get();
+		List<TaxonSet> taxonsets = m_set.get();
+		m_nIDs = new ArrayList[taxonsets.size()];
 		List<Sequence> data = m_taxa.get().m_pSequences.get();
-		m_nIDs = new ArrayList<Integer>();
-		for (Taxon taxon : set) {
-			String sLabel = taxon.getID();
-			int iTaxon = 0;
-			while (!data.get(iTaxon).m_sTaxon.get().equals(sLabel)) {
-				iTaxon++;
-				if (iTaxon == data.size()) {
-					throw new Exception("Unknown taxon (" + sLabel + ") in set (typo?)");
+		
+		for (int i = 0; i < taxonsets.size(); i++) {
+			TaxonSet taxonset = taxonsets.get(i);
+			m_nIDs[i] = new ArrayList<Integer>();
+			List<Taxon> set = taxonset.m_taxonset.get();
+			for (Taxon taxon : set) {
+				String sLabel = taxon.getID();
+				int iTaxon = 0;
+				while (!data.get(iTaxon).m_sTaxon.get().equals(sLabel)) {
+					iTaxon++;
+					if (iTaxon == data.size()) {
+						throw new Exception("Unknown taxon (" + sLabel + ") in set (typo?)");
+					}
 				}
+				m_nIDs[i].add(iTaxon);
 			}
-			m_nIDs.add(iTaxon);
+
 		}
+		
 	} // initAndValidate
 	
 	@Override
     public double calculateLogP() throws Exception {
-		if (isMonoPhyletic()) {
-			return 0.0;
+		for (int i = 0; i < m_nIDs.length; i++) {
+			if (!isMonoPhyletic(m_nIDs[i])) {
+				return Double.NEGATIVE_INFINITY;
+			}
 		}
-		return Double.NEGATIVE_INFINITY;
+		return 0.0;
 	}
 
 	/** Check whether the tree is monophyletic in the set of nodes
 	 * represented by m_nIDs.
 	 */
-	boolean isMonoPhyletic() {
+	boolean isMonoPhyletic(List<Integer> nIDs) {
 		Tree tree = m_tree.get();
 		Node [] nodes = tree.getNodesAsArray();
 		// mark all leaf nodes as being part of the clade
 		boolean [] isInClade = new boolean[nodes.length];
-		for (Integer iID : m_nIDs) {
+		for (Integer iID : nIDs) {
 			isInClade[iID] = true;
 		}
 		Set<Integer> set = new HashSet<Integer>();
-		set.addAll(m_nIDs);
+		set.addAll(nIDs);
 		while (set.size() > 1) {
 			// Find a node in the set that has a parent node with both
 			// children in the clade constructed so far.
