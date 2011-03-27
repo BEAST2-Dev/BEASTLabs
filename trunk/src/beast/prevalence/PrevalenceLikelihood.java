ac0619c7b97b75f8afb2d87cf544b4a7d521704d
higgs.ml <higgs.ml@a0f7aa36-7498-2ff5-2863-09083c5c2a5c>
2011-03-27 19:21:06 +0000

    prevalence update

--- trunk/src/beast/prevalence/PrevalenceLikelihood.java
+++ trunk/src/beast/prevalence/PrevalenceLikelihood.java
@@ -28,54 +28,55 @@ public class PrevalenceLikelihood extends Distribution {
 	
 	@Override
 	public double calculateLogP() {
-		logP = 0;
-		PrevalenceList list = m_list.get();
-		List<Item> items = list.getItems();
-		double beta = m_beta.get().getValue();
-		double gamma = m_gamma.get().getValue();
-		// RRB: is population size the definition of N?
-		int N = m_popSize.get().getValue();
-			
-		Double logP = 0.0;
-		Item start, finish;
-		// keep track of number infected
-		int ninf = 1; 
-				
-		// RRB: should this be "i >= 1" ???
-		for (int i= items.size()-1; i > 1; i--) {
-			start = items.get(i);
-			finish = items.get(i-1);
-		    // add contribution to likelihood of time spent in this period
-		    logP = logP -  (start.m_fTime - finish.m_fTime)*(beta*ninf*(N-ninf)/N + gamma*ninf);
-		    // check how current time period ends 
-		    switch  (finish.m_action) {
-		        case RECOVERED:
-		            // finishes with a recovery
-		            logP = logP + Math.log(gamma*ninf);
-		            ninf--;
-		            break;
-		        case INFECTED:
-		            // finishes with an infection
-		        	logP = logP + Math.log(beta*ninf*(N-ninf)/N);
-		        	ninf++;
-		        	break;
-		        case NONEVENT:
-		        	// no contribution from non-event
-		        	break;
-// this cannot happen due to the definition of the enum Action		        	
-//		        default:
-//		        	throw new Exception("Unrecognised event in item " + (i-1) + ". ");
-		    }
-		    		    		    
-		    // Check that the number infected is legal
-		    // RRB: so the number of infected cannot be zero anywhere in the list?
-		    if (ninf < 1 || ninf > N){
-		        logP = Double.NEGATIVE_INFINITY;
-		        return logP;
-		    }
-		}
-		return logP;
+        Double logP = 0.0;
+        PrevalenceList list = m_list.get();
+        List<Item> items = list.getItems();
+        double beta = m_beta.get().getValue();
+        double gamma = m_gamma.get().getValue();
+        // RRB: N is population size? Is population size an int?
+        // DW: yes, N is popSize and is a positive integer
+        int N = m_popSize.get().getValue();
+                
+        Item start, finish;
+        // keep track of number infected
+        int ninf = 1;
+                        
+        // RRB: should this be "i >= 1" ???
+        // DW: Oh yes, I forgot item(0) is the first in the list
+        for (int i= items.size()-1; i >= 1; i--) {
+                start = items.get(i);
+                finish = items.get(i-1);
+            // add contribution to likelihood of time spent in this period
+            logP = logP -  (start.m_fTime - finish.m_fTime)*(beta*ninf*(N-ninf)/N + gamma*ninf);
+            // check how current time period ends
+            switch  (finish.m_action) {
+                case RECOVERED:
+                    // finishes with a recovery
+                    logP = logP + Math.log(gamma*ninf);
+                    ninf--;
+                    break;
+                case INFECTED:
+                    // finishes with an infection
+                        logP = logP + Math.log(beta*ninf*(N-ninf)/N);
+                        ninf++;
+                        break;
+                case NONEVENT:
+                        // no contribution from non-event
+                        break;
+            }
+                                                
+            // Check that the number infected is legal
+            // RRB: so the number of infected cannot be zero anywhere in the list?
+            // DW: That's right
+            if (ninf < 1 || ninf > N){
+                logP = Double.NEGATIVE_INFINITY;
+                return logP;
+            }
+        }
+        return logP;
 	}
+	
+
 	
 	/** nonsense, feel free to ignore **/
 	@Override public List<String> getArguments() {return null;}
