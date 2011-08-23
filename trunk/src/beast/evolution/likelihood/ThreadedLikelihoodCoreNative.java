8a00f2356c1e9b0d46e34fe94e6b02c32fb01061
higgs.ml@gmail.com <higgs.ml@gmail.com@a0f7aa36-7498-2ff5-2863-09083c5c2a5c>
2011-08-23 01:42:28 +0000

    make scaling work in threaded likelihood core

--- trunk/src/beast/evolution/likelihood/ThreadedLikelihoodCoreNative.java
+++ trunk/src/beast/evolution/likelihood/ThreadedLikelihoodCoreNative.java
@@ -8,6 +8,14 @@ public class ThreadedLikelihoodCoreNative extends ThreadedLikelihoodCore {
 	int m_nNodes;
 	double SCALE = 1;
 
+	int m_nMatrixSize;
+	int m_nMatrixCount;
+	protected double[][] m_fMatrices;
+	protected int[] m_nMatrixID;
+	int m_nMatricesToUpdate = 0;
+    protected int[] m_iCurrentMatrices;
+    protected int[] m_iStoredMatrices;
+
     public ThreadedLikelihoodCoreNative(int nStateCount) {
     	m_nStates = nStateCount;
     } // c'tor
@@ -40,7 +48,16 @@ public class ThreadedLikelihoodCoreNative extends ThreadedLikelihoodCore {
 		
 		initializeC(m_pBEER, nNodeCount, nPatternCount, nMatrixCount, weights, 
 				iConstantPatterns2, nConstantPatterns, nThreads, bIntegrateCategories);
+
+       m_nMatrixSize = m_nStates * m_nStates;
+       m_nMatrixCount = nMatrixCount;
+       m_fMatrices = new double[nNodeCount][nMatrixCount * m_nMatrixSize];
+       m_nMatrixID = new int[nNodeCount];
+       m_iCurrentMatrices = new int[nNodeCount];
+       m_iStoredMatrices = new int[nNodeCount];
+       m_nMatricesToUpdate = 0;
 	}
+	
 	native boolean initializeC(long m_pBEER2, int nNodeCount, int nPatternCount, int nMatrixCount, int[] weights,
 			int[] iConstantPatterns2, int nConstantPatterns, int nThreads, boolean bIntegrateCategories);
     
@@ -78,12 +95,19 @@ public class ThreadedLikelihoodCoreNative extends ThreadedLikelihoodCore {
 
 	@Override
 	public void setNodeMatrixForUpdate(int iNode) {
+//        m_iCurrentMatrices[iNode] = 1 - m_iCurrentMatrices[iNode];
+//
+//        m_nMatrixID[m_nMatricesToUpdate] = iNode * 2 + m_iCurrentMatrices[iNode];
+//		m_nMatricesToUpdate++;
 		setNodeMatrixForUpdateC(m_pBEER, iNode);
 	}
 	native void setNodeMatrixForUpdateC(long pBEER, int iNode);
 
 	@Override
 	public void setNodeMatrix(int iNode, int iMatrixIndex, double[] fMatrix) {
+//        System.arraycopy(fMatrix, 0, m_fMatrices[m_nMatricesToUpdate - 1],
+//        		iMatrixIndex * m_nMatrixSize, m_nMatrixSize);
+		
 		setNodeMatrixC(m_pBEER, iNode, iMatrixIndex, fMatrix);
 	}
 	native void setNodeMatrixC(long pBeer, int iNode, int iMatrixIndex, double[] fMatrix);
@@ -127,18 +151,23 @@ public class ThreadedLikelihoodCoreNative extends ThreadedLikelihoodCore {
 
 	@Override
 	public void store() {
+        System.arraycopy(m_iStoredMatrices, 0, m_iCurrentMatrices, 0, m_nNodes);
 		storeC(m_pBEER);
 	}
 	native void storeC(long pBEER);
 
 	@Override
 	public void unstore() {
+        System.arraycopy(m_iStoredMatrices, 0, m_iCurrentMatrices, 0, m_nNodes);
 		unstoreC(m_pBEER);
 	}
 	native void unstoreC(long pBEER);
 
 	@Override
 	public void restore() {
+        int[] iTmp1 = m_iCurrentMatrices;
+        m_iCurrentMatrices = m_iStoredMatrices;
+        m_iStoredMatrices = iTmp1;
 		restoreC(m_pBEER);
 	}
 	native void restoreC(long pBEER);
@@ -164,8 +193,20 @@ public class ThreadedLikelihoodCoreNative extends ThreadedLikelihoodCore {
 			int [] cacheNode1, int [] cacheNode2, int [] cacheNode3, int cacheNodeCount, 
 			int iFrom, int iTo, int rootNodeNr, double[] proportions, double fProportionInvariant,
 			double[] frequencies) {
-		return calcLogPC(m_pBEER, iThread, cacheNode1, cacheNode2, cacheNode3, cacheNodeCount, iFrom, iTo, rootNodeNr, proportions, fProportionInvariant, frequencies);
+		for (int i = 0; i <  m_nMatricesToUpdate; i++) {
+			setUpMatrixC(m_pBEER, m_fMatrices[i], m_nMatrixID[i]);
+		}
+		m_nMatricesToUpdate = 0;
+//		if (m_nMatricesToUpdate > 0) {
+//			setUpMatricesC(m_pBEER, m_fMatrices, m_nMatrixID, m_nMatricesToUpdate);
+//			m_nMatricesToUpdate = 0;
+//		}
+		
+		double logP = calcLogPC(m_pBEER, iThread, cacheNode1, cacheNode2, cacheNode3, cacheNodeCount, iFrom, iTo, rootNodeNr, proportions, fProportionInvariant, frequencies);
+		return logP;
 	}
+	native void setUpMatricesC(long pBEER, double [] fMatrices, int [] nMatrixID, int nMatrixToUpdate);
+	native void setUpMatrixC(long pBEER, double [] fMatrices, int nMatrixID);
 
 	native double calcLogPC(long pBEER, int iThread, 
 			int [] cacheNode1, int [] cacheNode2, int [] cacheNode3, int cacheNodeCount, 
