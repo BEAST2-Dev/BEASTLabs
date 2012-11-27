391a3b06ec330b022e97182ac8ef2d1fc37873f4
higgs.ml@gmail.com <higgs.ml@gmail.com@a0f7aa36-7498-2ff5-2863-09083c5c2a5c>
2012-11-27 23:22:37 +0000

    add path sampler

---
+++ trunk/src/beast/inference/PathSampleAnalyser.java
@@ -0,0 +1,58 @@
+package beast.inference;
+
+import java.text.DecimalFormat;
+
+import beast.core.Description;
+import beast.core.Plugin;
+import beast.util.LogAnalyser;
+
+@Description("Reads logs produces through PathSamples and estimates marginal likelihood")
+public class PathSampleAnalyser extends Plugin {
+	
+	
+	DecimalFormat formatter;
+	
+	@Override
+	public void initAndValidate() throws Exception {
+	}
+	
+	double estimateMarginalLikelihood(int nSteps, double alpha, String rootDir, int burnInPercentage) throws Exception {
+		String sFormat = "";
+		for (int i = nSteps; i > 0; i /= 10) {
+			sFormat += "#";
+		}
+		formatter = new DecimalFormat(sFormat);
+
+		// collect likelihood estimates for each step
+		double [] marginalLs = new double[nSteps];
+		for (int i = 0; i < nSteps; i++) {
+			String logFile = getParticleDir(rootDir, i) + "/" + PathSampler.LIKELIHOOD_LOG_FILE;
+			LogAnalyser analyser = new LogAnalyser(new String[] {logFile}, 2000, burnInPercentage);
+			marginalLs[i] = analyser.getMean("likelihood");
+			System.err.println("marginalLs[" + i + " ] = " + marginalLs[i]);
+		}
+		
+		// combine steps
+		double marginalL = 0;
+		for (int i = 0; i < nSteps - 1; i++) {
+			marginalL += marginalLs[i] + marginalLs[i + 1]; 
+		}		
+		marginalL = marginalL / (2.0 * (nSteps - 1));
+		return marginalL;
+	}
+
+	String getParticleDir(String rootDir, int iParticle) {
+		return rootDir + "/step" + formatter.format(iParticle);
+	}
+	
+	public static void main(String[] args) throws Exception {
+		PathSampleAnalyser analyser = new PathSampleAnalyser();
+		int nSteps = Integer.parseInt(args[0]);
+		double alpha = Double.parseDouble(args[1]);
+		String rootDir = args[2];
+		int burnInPercentage = Integer.parseInt(args[3]);
+		double marginalL = analyser.estimateMarginalLikelihood(nSteps, alpha, rootDir, burnInPercentage);
+		System.out.println("marginal L estimate =" + marginalL);
+	}
+	
+}
---
+++ trunk/src/beast/inference/PathSamplingStep.java
@@ -0,0 +1,148 @@
+package beast.inference;
+
+import java.util.List;
+
+import beast.core.Description;
+import beast.core.Distribution;
+import beast.core.Evaluator;
+import beast.core.Input;
+import beast.core.MCMC;
+import beast.core.Operator;
+import beast.core.util.CompoundDistribution;
+import beast.util.Randomizer;
+
+@Description("Calculate marginal likelihood through path sampling for a single step")
+public class PathSamplingStep extends MCMC {
+
+	public Input<Double> betaInput = new Input<Double>("beta","power used for likelihood: 1 = using full posterior, 0 = using prior only", 1.0);
+
+	double beta;
+	Distribution prior;
+	Distribution likelihood;
+	
+	@Override
+	public void initAndValidate() throws Exception {
+		super.initAndValidate();
+		
+		beta = betaInput.get();
+		posterior = posteriorInput.get();
+		// expect compound distribution with likelihood and prior
+		if (!(posterior instanceof CompoundDistribution)) {
+			throw new Exception("Expected posterior being a CompoundDistribution");
+		}
+		CompoundDistribution d = (CompoundDistribution) posterior;
+		List<Distribution> list = d.pDistributions.get();
+		if (list.size() != 2) {
+			throw new Exception("Expected posterior with only likelihood and prior as distributions");
+		}
+		if (list.get(0).getID().toLowerCase().startsWith("likelihood")) {
+			prior = list.get(1);
+			likelihood = list.get(0);
+		} else {
+			if (list.get(0).getID().toLowerCase().startsWith("prior")) {
+				prior = list.get(0);
+				likelihood = list.get(1);
+			} else {
+				throw new Exception("Expected posterior with only likelihood and prior as IDs");
+			}
+		}
+	}
+	
+    /**
+     * main MCMC loop *
+     */
+    protected void doLoop() throws Exception {
+    	
+        double logPriorProb = prior.calculateLogP();
+        double logLikelihood = likelihood.calculateLogP();        
+        fOldLogLikelihood = logPriorProb + logLikelihood * beta; 
+    	
+    	
+        for (int iSample = -nBurnIn; iSample <= nChainLength; iSample++) {
+            final int currentState = iSample;
+
+            state.store(currentState);
+            if (m_nStoreEvery > 0 && iSample % m_nStoreEvery == 0 && iSample > 0) {
+                state.storeToFile(iSample);
+            	operatorSchedule.storeToFile();
+            }
+
+            Operator operator = operatorSchedule.selectOperator();
+            //System.out.print("\n" + iSample + " " + operator.getName()+ ":");
+
+            final Distribution evaluatorDistribution = operator.getEvaluatorDistribution();
+            Evaluator evaluator = null;
+
+            if (evaluatorDistribution != null) {
+                evaluator = new Evaluator() {
+                    @Override
+                    public double evaluate() {
+                        double logP = 0.0;
+
+                        state.storeCalculationNodes();
+                        state.checkCalculationNodesDirtiness();
+
+                        try {
+                            logP = evaluatorDistribution.calculateLogP();
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                            System.exit(1);
+                        }
+
+                        state.restore();
+                        state.store(currentState);
+
+                        return logP;
+                    }
+                };
+            }
+
+            double fLogHastingsRatio = operator.proposal(evaluator);
+
+            if (fLogHastingsRatio != Double.NEGATIVE_INFINITY) {
+
+                state.storeCalculationNodes();
+                state.checkCalculationNodesDirtiness();
+
+                
+                logPriorProb = prior.calculateLogP();
+                logLikelihood = likelihood.calculateLogP();
+                
+                fNewLogLikelihood = logPriorProb + logLikelihood * beta; 
+
+                logAlpha = fNewLogLikelihood - fOldLogLikelihood + fLogHastingsRatio; //CHECK HASTINGS
+                //System.out.println(logAlpha + " " + fNewLogLikelihood + " " + fOldLogLikelihood);
+                if (logAlpha >= 0 || Randomizer.nextDouble() < Math.exp(logAlpha)) {
+                    // accept
+                    fOldLogLikelihood = fNewLogLikelihood;
+                    state.acceptCalculationNodes();
+
+                    if (iSample >= 0) {
+                        operator.accept();
+                    }
+                    //System.out.print(" accept");
+                } else {
+                    // reject
+                    if (iSample >= 0) {
+                        operator.reject();
+                    }
+                    state.restore();
+                    state.restoreCalculationNodes();
+                    //System.out.print(" reject");
+                }
+                state.setEverythingDirty(false);
+            } else {
+                // operation failed
+                if (iSample >= 0) {
+                    operator.reject();
+                }
+                state.restore();
+                //System.out.print(" direct reject");
+            }
+            log(iSample);
+
+            operator.optimize(logAlpha);
+            callUserFunction(iSample);
+        }
+    }
+}
