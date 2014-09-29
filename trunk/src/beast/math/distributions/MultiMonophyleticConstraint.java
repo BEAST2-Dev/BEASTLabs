6ef1bf802ae1a177a90ff8ab9c4b22f717bd89e8
jheled@gmail.com <jheled@gmail.com@a0f7aa36-7498-2ff5-2863-09083c5c2a5c>
2014-09-29 06:59:25 +0000

    New Pruned tree and alignment. New random tree with constraints. Check all monophyly constraints in one (preorder) tree pass to speed up checking many constraints

--- trunk/src/beast/math/distributions/MultiMonophyleticConstraint.java
+++ trunk/src/beast/math/distributions/MultiMonophyleticConstraint.java
@@ -1,10 +1,6 @@
 package beast.math.distributions;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Random;
-import java.util.Set;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -33,16 +29,80 @@ public class MultiMonophyleticConstraint extends Distribution {
 
     Tree tree;
     String[] taxaList;
-    
+
+    // indexed by node number, the (smallest monophyletic) clade this leaf belongs to (0 .. #clades). -1 for free taxa.
+    int[] leafCladeAssignments;
+    // cladeSize[i] is the size (number of taxa) in the i'th clade.
+    int[] cladeSize;
+    // cladeParent[i] is the clade assignment of the embedding monophyletic clade (-1 for none)
+    int[] cladeParent;
+
     @Override
     public void initAndValidate() throws Exception {
-    	taxonIDList = new ArrayList<List<Integer>>();
-    	tree = treeInput.get();
-    	taxaList = tree.getTaxaNames();
+        taxonIDList = new ArrayList<List<Integer>>();
+        tree = treeInput.get();
+        taxaList = tree.getTaxaNames();
 
-    	parse(newickInput.get());
+        parse(newickInput.get());
+
+        // setup internal arrays for monophyly check
+        leafCladeAssignments = new int[tree.getNodeCount()];
+        Arrays.fill(leafCladeAssignments, -1);
+
+        cladeSize = new int[taxonIDList.size()];
+        cladeParent = new int[taxonIDList.size()];
+        Arrays.fill(cladeParent, -1);
+
+        Node [] nodes = tree.getNodesAsArray();
+        for(int k = 0; k < taxonIDList.size(); ++k ) {
+            cladeSize[k] = taxonIDList.get(k).size();
+        }
+
+        for(int k = 0; k < taxonIDList.size(); ++k ) {
+            final List<Integer> t = taxonIDList.get(k);
+            for( int n : t ) {
+                final int i = nodes[n].getNr();
+                if( leafCladeAssignments[i] == -1 ) {
+                    leafCladeAssignments[i] = k;
+                } else {
+                    // nesting situation k and leafCladeAssignments[i]; keep the assignment to the smaller clade
+                    if( cladeSize[leafCladeAssignments[i]] > cladeSize[k] ) {
+                        // [i] is parent of k
+                        leafCladeAssignments[i] = k;
+                    }
+                }
+            }
+        }
+        // slow but who cares - one time only
+        for(int k = 0; k < taxonIDList.size(); ++k ) {
+            final List<Integer> tk = taxonIDList.get(k);
+            for(int i = 0; i < k; ++i ) {
+                final List<Integer> ti = taxonIDList.get(i);
+                for( int n : ti ) {
+                    if( tk.contains(n) ) {
+                        updateCladeParent(i, k);
+                        break;
+                    }
+                }
+            }
+        }
     }
-    
+
+    private void updateCladeParent(int subClade, int clade) {
+        if( cladeSize[subClade] > cladeSize[clade] ) {
+            int tmp = subClade;
+            subClade = clade;
+            clade = tmp;
+        }
+
+        if( cladeParent[subClade] != clade ) {
+            if( cladeParent[subClade] == -1 ||
+                    cladeSize[cladeParent[subClade]] > cladeSize[clade] ) {
+                cladeParent[subClade] = clade;
+            }
+        }
+    }
+
 	/** extract clades from Newick string,
 	 * and add constraints for all internal nodes (except the root if it contains all taxa)
 	 **/
@@ -67,10 +127,11 @@ public class MultiMonophyleticConstraint extends Distribution {
 					int i = indexOf(taxon);
 					if (i == -1 && (taxon.startsWith("'") || taxon.startsWith("\""))) {
 						i = indexOf(taxon.substring(1, taxon.length() - 1));
-						if (i == -1) {
-							throw new RuntimeException("Cannot find taxon " + taxon + "  in taxon list");
-						}
+
 					}
+                    if (i == -1) {
+                        throw new RuntimeException("Cannot find taxon " + taxon + "  in taxon list");
+                    }
 					list.add(i);
 				}
 			}
@@ -97,16 +158,22 @@ public class MultiMonophyleticConstraint extends Distribution {
 
 	@Override
     public double calculateLogP() throws Exception {
-        logP = 0;
-        for (List<Integer> list : taxonIDList) {
-        	if (!isMonophyletic(list)) {
-        		logP = Double.NEGATIVE_INFINITY;
-        		return logP;
-        	}
-        }
+        final boolean mono1 = isMonoJH();
+        if( false ) assert mono1 == isMonoRB();   // assert is expensive. isMonoJH replaces the much slower isMonoRB
+
+        logP = mono1 ? 0 : Double.NEGATIVE_INFINITY;
         return logP;
     }
 
+
+    private boolean isMonoRB() {
+        for (List<Integer> list : taxonIDList) {
+            if( !isMonophyletic(list) ) {
+                return false;
+            }
+        }
+        return true;
+    }
 
     /** walk up the tree from the nodes, to see whether a clade is monophyletic
      * After initialising steps from each of the child nodes,
@@ -143,14 +210,133 @@ public class MultiMonophyleticConstraint extends Distribution {
 			}
 			list2 = list3;
 		}
-		if (k == list.size() - 1){
-			return true;
-		}
-		return false;
-	}
+        return k == list.size() - 1;
+    }
 
+    /**
+     *  Check all monophyly constraints simultaneously in one (post-order) pass.
+     *  A monophyly violation is detected when a subtree contains taxa from two incompatible clades,
+     *  and then the function immediately returns false.
+     *  Each taxon is assigned a clade index/assignment, the smallest clade it belongs to.
+     *  A clade count is calculated for each internal node, that is the number of taxa in the current clade it is
+     *  embedding in. The node inherits the clade index/assignment from its children. When the root of a clade is
+     *  reached, i.e. when the count is equal to the known size of the clade, its clade assignment is updated to be the
+     *  assignment of its parent (if any).
+     *
+     * @return  true if all monophyly constraints are met, false otherwise
+     */
+    private boolean isMonoJH() {
+        // this traversal is shared between all calling code in current cycle.
+        Node[] post = tree.listNodesPostOrder(null, null);
+        final int nNodes = post.length;
+        // per tree node, number of taxa in current monophyletic clade
+        int nodeCladeSize[] = new int[nNodes];
+        // per tree node, clade index (or assignment) of taxa in this sub tree.
+        int[] nodeClade = new int[nNodes];
+
+        for (final Node n : post) {
+            final int nr = n.getNr();
+            if( n.isLeaf() ) {
+                nodeClade[nr] = leafCladeAssignments[nr];
+                if( nodeClade[nr] >= 0 ) {
+                    nodeCladeSize[nr] = 1;
+                    // a leaf in a clade of its own is ipso facto completed.
+                    if( cladeSize[nodeClade[nr]] == 1 ) {
+                        nodeClade[nr] = cladeParent[nodeClade[nr]];
+                    }
+                }
+            } else {
+                final int lnr = n.getLeft().getNr();
+                final int l = nodeClade[lnr];
+                final int rnr = n.getRight().getNr();
+                final int r = nodeClade[rnr];
+
+                if( l != r ) {
+                    // A node with mixed taxa (from two mono clades or one clade and free taxa)
+                    return false;
+                }
+
+                nodeClade[nr] = l;
+
+                if( l != -1 ) {
+                    nodeCladeSize[nr] = nodeCladeSize[lnr] + nodeCladeSize[rnr];
+                    if( nodeCladeSize[nr] == cladeSize[l] ) {
+                        // clade root: set its assignment to parent, if any.
+                        nodeClade[nr] = cladeParent[l];
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    public int getMonoNodes(boolean[] nodes) {
+
+        // this traversal is shared between all calling code in current cycle.
+        Node[] post = tree.listNodesPostOrder(null, null);
+        final int nNodes = post.length;
+
+        assert nNodes == nodes.length;
+        Arrays.fill(nodes, false);
+
+        // per tree node, number of taxa in current monophyletic clade
+        int nodeCladeSize[] = new int[nNodes];
+        // per tree node, clade index (or assignment) of taxa in this sub tree.
+        int[] nodeClade = new int[nNodes];
+        int nMono = 0;
+        for (final Node n : post) {
+            final int nr = n.getNr();
+            if( n.isLeaf() ) {
+                nodeClade[nr] = leafCladeAssignments[nr];
+                if( nodeClade[nr] >= 0 ) {
+                    nodeCladeSize[nr] = 1;
+                    // a leaf in a clade of its own is ipso facto completed.
+                    if( cladeSize[nodeClade[nr]] == 1 ) {
+                        nodeClade[nr] = cladeParent[nodeClade[nr]];
+                    }
+                }
+            } else {
+                final int lnr = n.getLeft().getNr();
+                final int l = nodeClade[lnr];
+                final int rnr = n.getRight().getNr();
+                final int r = nodeClade[rnr];
+
+                if( l != r ) {
+                    // A node with mixed taxa (from two mono clades or one clade and free taxa)
+                    return -1;
+                }
+
+                nodeClade[nr] = l;
+
+                if( l != -1 ) {
+                    nodeCladeSize[nr] = nodeCladeSize[lnr] + nodeCladeSize[rnr];
+                    if( nodeCladeSize[nr] == cladeSize[l] ) {
+                        nodes[n.getNr()] = true;
+                        nMono += 1;
+                        // clade root: set its assignment to parent, if any.
+                        nodeClade[nr] = cladeParent[l];
+                    }
+                }
+            }
+        }
+        return nMono;
+    }
 
 	@Override public List<String> getArguments() {return null;}
 	@Override public List<String> getConditions() {return null;}
 	@Override public void sample(State state, Random random) {}
+
+    /** Return constraints as a collection of clade tip sets (tips taxa as strings) **/
+
+    public List<List<String>> getConstraints() {
+        List<List<String>> allc = new ArrayList<List<String>>();
+        for (List<Integer> list : taxonIDList) {
+            List<String> tax = new ArrayList<>(list.size());
+            for( int i : list ) {
+                tax.add(taxaList[i]);
+            }
+            allc.add(tax);
+        }
+        return allc;
+    }
 }
