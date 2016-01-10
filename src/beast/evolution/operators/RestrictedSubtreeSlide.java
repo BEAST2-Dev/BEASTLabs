4be239dbaac3f9d435a64e28272392d84d75de64
Remco Bouckaert <higgs.ml@gmail.com>
2016-01-11 12:46:19 +1300

    sync with beast2

--- src/beast/evolution/operators/RestrictedSubtreeSlide.java
+++ src/beast/evolution/operators/RestrictedSubtreeSlide.java
@@ -390,9 +390,9 @@ public class RestrictedSubtreeSlide extends SubtreeSlide {
 
     private double getDelta() {
         if (!gaussianInput.get()) {
-            return (Randomizer.nextDouble() * fSize) - (fSize / 2.0);
+            return (Randomizer.nextDouble() * size) - (size / 2.0);
         } else {
-            return Randomizer.nextGaussian() * fSize;
+            return Randomizer.nextGaussian() * size;
         }
     }
 
