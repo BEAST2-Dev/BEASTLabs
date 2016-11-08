e07fc7df4f7d1b22f021eb4b6ca3de9b0557c892
Remco Bouckaert <higgs.ml@gmail.com>
2016-11-09 10:08:10 +1300

    add CompoundOperator + test

---
+++ src/beast/evolution/operators/CompoundOperator.java
@@ -0,0 +1,142 @@
+package beast.evolution.operators;
+
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import beast.core.Description;
+import beast.core.Input;
+import beast.core.Input.Validate;
+import beast.core.util.Log;
+import beast.core.Operator;
+import beast.core.OperatorSchedule;
+import beast.core.StateNode;
+import beast.util.Randomizer;
+
+@Description("Operator that distributes a certain amount of operator weight. "
+		+ "This is useful in setting up analyses with many operators (like a *BEAST analysis) "
+		+ "and where the total amount of weight for groups of operators (say gene tree operators) "
+		+ "needs to be fixed for optimal performance.")
+public class CompoundOperator extends Operator {
+	public Input<List<Operator>> operatorInput = new Input<>("operator", "operator the CompoundOperator chooses from with probability proportional to its weight", new ArrayList<>(), Validate.REQUIRED);
+	
+    private List<Operator> operators = new ArrayList<>();
+
+    /** last operator used -- record the last choice for parameter tuning **/
+    public Operator lastOperator;
+
+    /**
+     * cumulative weights, with unity as max value *
+     */
+    private double[] cumulativeProbs;
+    
+    
+	@Override
+	public void initAndValidate() {
+		operators = operatorInput.get();
+
+		// collect weights
+		cumulativeProbs = new double[operators.size()];
+		cumulativeProbs[0] = operators.get(0).getWeight();
+		for (int i = 1; i < operators.size(); i++) {
+			cumulativeProbs[i] = operators.get(i).getWeight() + cumulativeProbs[i-1];
+		}
+		// normalise
+		for (int i = 0; i < operators.size(); i++) {
+			cumulativeProbs[i] /= cumulativeProbs[operators.size() - 1];
+		}
+	}
+
+	@Override
+	public double proposal() {
+        final int operatorIndex = Randomizer.randomChoice(cumulativeProbs);
+        lastOperator = operators.get(operatorIndex);
+		return lastOperator.proposal();
+	}
+	
+	
+	@Override
+	public void optimize(double logAlpha) {
+		lastOperator.optimize(logAlpha);
+	}
+	
+	@Override
+	public void accept() {
+		lastOperator.accept();
+	}
+	
+	@Override
+	public void reject() {
+		lastOperator.reject();
+	}
+	
+	@Override
+	public List<StateNode> listStateNodes() {
+		List<StateNode> list = new ArrayList<StateNode>();
+		for (Operator operator : operators) {
+			List<StateNode> list2 = operator.listStateNodes();
+			list.addAll(list2);
+			
+		}
+		return list;
+	}
+
+	@Override
+	public void storeToFile(PrintWriter out) {
+		out.print("{\"id\":\"" + getID() + "\",\"operators\":[\n");
+		int k = 0;
+		for (Operator o : operators) {
+			o.storeToFile(out);
+            if (k++ < operators.size() - 1) {
+            	out.println(",");
+            }
+		}
+        out.print("]}");
+	}
+	
+	
+	@Override
+	public void restoreFromFile(JSONObject o) {
+        try {
+        JSONArray operatorlist = o.getJSONArray("operators");
+        for (int i = 0; i < operatorlist.length(); i++) {
+            JSONObject item = operatorlist.getJSONObject(i);
+            String id = item.getString("id");
+    		boolean found = false;
+            if (!id.equals("null")) {
+            	for (Operator operator: operators) {
+            		if (id.equals(operator.getID())) {
+                    	operator.restoreFromFile(item);
+                        found = true;
+            			break;
+            		}
+            	}
+            }
+        	if (!found) {
+        		Log.warning.println("Operator (" + id + ") found in state file that is not in operator list any more");
+        	}
+        }
+    	for (Operator operator: operators) {
+    		if (operator.getID() == null) {
+        		Log.warning.println("Operator (" + operator.getClass() + ") found in BEAST file that could not be restored because it has not ID");
+    		}
+    	}
+        } catch (JSONException e) {
+        	// it is not a JSON file -- probably a version 2.0.X state file
+	    }
+	}
+	
+	
+	@Override
+	public void setOperatorSchedule(final OperatorSchedule operatorSchedule) {
+        for (Operator o : operators) {
+        	o.setOperatorSchedule(operatorSchedule);
+        }
+    }
+
+
+}
---
+++ src/test/beast/evolution/operators/CompoundOperatorTest.java
@@ -0,0 +1,146 @@
+package test.beast.evolution.operator;
+
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import org.junit.Test;
+
+import beast.app.beauti.BeautiDoc;
+import beast.core.Operator;
+import beast.core.OperatorSchedule;
+import beast.core.State;
+import beast.core.StateNode;
+import beast.core.parameter.RealParameter;
+import beast.evolution.operators.CompoundOperator;
+import beast.evolution.operators.DeltaExchangeOperator;
+import beast.evolution.operators.ScaleOperator;
+import junit.framework.TestCase;
+
+public class CompoundOperatorTest extends TestCase {
+	
+	@Test
+	public void testCompoundOperator() {
+		DeltaExchangeOperator operator1 = new DeltaExchangeOperator();
+		operator1.setID("deltaOperator");
+		RealParameter parameter = new RealParameter(new Double[] {1., 1., 1., 1.});
+		operator1.initByName("parameter", parameter, "weight", 1.0);
+
+		ScaleOperator operator2 = new ScaleOperator(); 
+		operator2.setID("scaleOperator");
+		operator2.initByName("parameter", parameter, "weight", 3.0);
+
+		CompoundOperator co = new CompoundOperator();
+		co.setID("compoundOperator");
+		co.initByName("operator", operator1, "operator", operator2, "weight", 1.0);
+
+		State state = new State();
+		state.initByName("stateNode", parameter);
+		state.initialise();
+		
+
+		OperatorSchedule schedule = new OperatorSchedule();
+		schedule.addOperator(co);
+		schedule.initAndValidate();
+		
+		Operator o = schedule.selectOperator();
+		assertEquals(o.getClass(), CompoundOperator.class);
+		
+		// test proposal, accept, optimize
+		int scaleOperatorCount0 = 0;
+		for (int i = 0; i < 1000; i++) {
+			o.proposal();
+			if (((CompoundOperator)o).lastOperator instanceof ScaleOperator) {
+				scaleOperatorCount0++;
+			}
+			o.accept();
+			o.optimize(1);
+		}
+		assertTrue(scaleOperatorCount0 > 700 && scaleOperatorCount0 < 800);
+		
+		// test store & restore
+		try {
+			String stateFile = "/tmp/x.state";
+			if ((new File(stateFile)).exists()) {
+				(new File(stateFile)).delete();
+			}
+			state.setStateFileName(stateFile);
+			state.storeToFile(123);
+			schedule.setStateFileName(stateFile);
+			schedule.storeToFile();
+			schedule.restoreFromFile();
+
+			String s = BeautiDoc.load(stateFile);
+			matches(s,"\"accept\":" + scaleOperatorCount0 + ",\"reject\":" + 0 + ",");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		// test proposal, reject, optimize
+		int scaleOperatorCount = 0;
+		for (int i = 0; i < 1000; i++) {
+			o.proposal();
+			if (((CompoundOperator)o).lastOperator instanceof ScaleOperator) {
+				scaleOperatorCount++;
+			}
+			o.reject();
+			o.optimize(1);
+		}
+		assertTrue(scaleOperatorCount > 700 && scaleOperatorCount < 800);
+
+		// test store & restore
+		try {
+			String stateFile = "/tmp/x.state2";
+			if ((new File(stateFile)).exists()) {
+				(new File(stateFile)).delete();
+			}
+			state.setStateFileName(stateFile);
+			state.storeToFile(123);
+			schedule.setStateFileName(stateFile);
+			schedule.storeToFile();			
+			String s = BeautiDoc.load(stateFile);
+			matches(s,"\"accept\":" + scaleOperatorCount0 + ",\"reject\":" + scaleOperatorCount + ",");
+			matches(s,"\"accept\":" + (1000-scaleOperatorCount0) + ",\"reject\":" + (1000 -scaleOperatorCount) + ",");
+			
+			stateFile = "/tmp/x.state";
+			schedule.setStateFileName(stateFile);
+			schedule.restoreFromFile();
+			s = BeautiDoc.load(stateFile);
+			matches(s,"\"accept\":" + scaleOperatorCount0 + ",\"reject\":" + 0 + ",");
+			matches(s,"\"accept\":" + (1000-scaleOperatorCount0) + ",\"reject\":" + 0 + ",");
+			
+
+			stateFile = "/tmp/x.state2";
+			if ((new File(stateFile)).exists()) {
+				(new File(stateFile)).delete();
+			}
+			state.setStateFileName(stateFile);
+			state.storeToFile(123);
+			schedule.setStateFileName(stateFile);
+			schedule.storeToFile();
+			s = BeautiDoc.load(stateFile);
+			matches(s,"\"accept\":" + scaleOperatorCount0 + ",\"reject\":" + 0 + ",");
+			matches(s,"\"accept\":" + (1000-scaleOperatorCount0) + ",\"reject\":" + 0 + ",");
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+
+		// test listStateNodes
+		List<StateNode> list = co.listStateNodes();
+		assertEquals(2,list.size());
+		assertEquals(parameter, list.get(0));
+		assertEquals(parameter, list.get(1));
+		
+	}
+
+	private void matches(String s, String match) {
+		int i = s.indexOf(match);
+		assertTrue(i > 0);
+	}
+
+}
