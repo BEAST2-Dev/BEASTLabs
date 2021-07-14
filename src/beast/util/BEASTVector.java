89228c70704c4637409d1dc704245e9614dd4954
Walter <walterxie@users.noreply.github.com>
2021-07-15 10:30:02 +1200

    Add new Distribution and BEASTVector #11

---
+++ src/beast/util/BEASTVector.java
@@ -0,0 +1,31 @@
+package beast.util;
+
+import beast.core.BEASTInterface;
+import beast.core.BEASTObject;
+import beast.core.Input;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BEASTVector extends BEASTObject {
+
+    public Input<List<BEASTInterface>> vectorInput = new Input<>("element", "a vector of beast objects.", new ArrayList<>());
+
+    public BEASTVector(List<BEASTInterface> elements, String id) {
+        setInputValue("element", elements);
+        initAndValidate();
+        setID(id);
+    }
+
+    public BEASTVector(List<BEASTInterface> elements) {
+        setInputValue("element", elements);
+        initAndValidate();
+    }
+
+    @Override
+    public void initAndValidate() {}
+
+    public List<BEASTInterface> getObjectList() {
+        return vectorInput.get();
+    }
+}
