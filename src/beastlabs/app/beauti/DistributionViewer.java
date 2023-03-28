9e323ec4c200130dc032eff133845e0f0f505a5a
Remco Bouckaert <higgs.ml@gmail.com>
2023-03-28 13:35:11 +1300

    fix distribution viewer for javafx components

--- src/beastlabs/app/beauti/DistributionViewer.java
+++ src/beastlabs/app/beauti/DistributionViewer.java
@@ -1,19 +1,9 @@
 package beastlabs.app.beauti;
 
 
-import java.awt.Component;
 import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
-import javax.swing.BoxLayout;
-import javax.swing.JButton;
-import javax.swing.JComboBox;
-import javax.swing.JFrame;
-import javax.swing.JLayeredPane;
-import javax.swing.JPanel;
-import javax.swing.JRootPane;
-import javax.swing.WindowConstants;
 import javax.xml.parsers.ParserConfigurationException;
 
 import org.xml.sax.SAXException;
@@ -22,104 +12,74 @@ import beastfx.app.inputeditor.BeautiDoc;
 import beastfx.app.inputeditor.BeautiSubTemplate;
 import beast.base.parser.PartitionContext;
 import beastfx.app.inputeditor.InputEditor;
+import beastfx.app.util.FXUtils;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 import beast.base.inference.parameter.RealParameter;
+import beast.base.inference.StateNode;
 import beast.base.inference.distribution.Prior;
 import beast.base.inference.distribution.Uniform;
 import beast.base.parser.XMLParserException;
 
-public class DistributionViewer {
+public class DistributionViewer extends javafx.application.Application {
 
 	BeautiDoc doc;
 	
-		public DistributionViewer() throws XMLParserException, SAXException, IOException, ParserConfigurationException {
-			doc = new BeautiDoc();
-			doc.parseArgs(new String[]{});
-			
-            JFrame frame = new JFrame("Parametric distribution");
-            // start with prior with uniform(0,1) distribution
-            final Prior prior = new Prior();
-            Uniform uniform = new Uniform();
-            uniform.setID("Uniform.0");
-            uniform.initByName("lower","0.0","upper","1.0");
-            prior.initByName("x", new RealParameter("0.0"), "distr", uniform);
-            prior.setID("Parametric.Distribution");
-            
-            // create panel with parametric distribution viewer
-            refreshDistributionPanel(frame, prior);
+	public DistributionViewer() throws XMLParserException, SAXException, IOException, ParserConfigurationException {
+		doc = new BeautiDoc();
+		doc.parseArgs(new String[]{});
+		doc.beautiConfig.suppressBEASTObjects.add(RealParameter.class.getName() + ".estimate");
+		doc.beautiConfig.suppressBEASTObjects.add(StateNode.class.getName() + ".estimate");
+	}
 
-            frame.setSize(800, 400);
-            frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-            frame.setVisible(true);
-        }
-
-        JPanel panel = null;
+	@Override
+	public void start(Stage primaryStage) throws Exception {
 		
-		private void refreshDistributionPanel(JFrame frame, Prior prior) {
-			// clear frame content in case it is refreshed after change of distribution  
-			JRootPane p = (JRootPane) frame.getComponent(0);
-			JLayeredPane p1 = (JLayeredPane) p.getComponent(1);
-			JPanel p2 = (JPanel) p1.getComponent(0);
-			p2.removeAll();
-			
-			if (panel != null) {
-				panel.getParent().remove(panel);
-			}
-			
-            panel = new JPanel();
-            panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
-			
-            // add combobox with parametric distributions
-            List<BeautiSubTemplate> availableBEASTObjects = doc.getInputEditorFactory().getAvailableTemplates(prior.distInput, prior, null, doc);
-            JComboBox<BeautiSubTemplate> comboBox = new JComboBox<BeautiSubTemplate>(availableBEASTObjects.toArray(new BeautiSubTemplate[]{}));
-            panel.add(comboBox);
-            
-            String id = prior.distInput.get().getID();
-            id = id.substring(0, id.indexOf('.'));
-            for (BeautiSubTemplate template : availableBEASTObjects) {
-                if (template.classInput.get() != null && template.shortClassName.equals(id)) {
-                    comboBox.setSelectedItem(template);
-                }
-            }
-            
-            try {
-            	// add parametric input editor
-                final InputEditor editor = doc.inputEditorFactory.createInputEditor(prior.distInput, prior, doc);
-                panel.add((Component) editor);
-                comboBox.addActionListener(e -> {
-                    @SuppressWarnings("unchecked")
-        			JComboBox<BeautiSubTemplate> comboBox1 = (JComboBox<BeautiSubTemplate>) e.getSource();
+        final Prior prior = new Prior();
+        Uniform uniform = new Uniform();
+        uniform.setID("Uniform.0");
+        uniform.initByName("lower","0.0","upper","1.0");
+        RealParameter parameter = new RealParameter("0.0");
+        parameter.setBounds(Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY);
+        prior.initByName("x", parameter, "distr", uniform);
+        prior.setID("Parametric.Distribution");
 
-                    BeautiSubTemplate template = (BeautiSubTemplate) comboBox1.getSelectedItem();
-                    try {
-                        template.createSubNet(new PartitionContext(), prior, prior.distInput, true);
-                    } catch (Exception e1) {
-                        e1.printStackTrace();
-                    }
-                    refreshDistributionPanel(frame, prior);
-                });
-			} catch (NoSuchMethodException | SecurityException | ClassNotFoundException | InstantiationException
-					| IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
-				e.printStackTrace();
-			}
-            
-            // add close button
-            JButton closeButton = new JButton("Close");
-            closeButton.addActionListener(e -> {
-            	frame.setVisible(false);
-            	frame.dispose();
-            });
-            panel.add(closeButton);
-			
-            frame.add(panel);
-            frame.revalidate();
-		}
+        // add combobox with parametric distributions
+		VBox box = FXUtils.newVBox();
+        List<BeautiSubTemplate> availableBEASTObjects = doc.getInputEditorFactory().getAvailableTemplates(prior.distInput, prior, null, doc);
+        ComboBox<BeautiSubTemplate> comboBox = new ComboBox<>();
+        comboBox.getItems().addAll(availableBEASTObjects.toArray(new BeautiSubTemplate[]{}));
+        
+        box.getChildren().add(comboBox);
+        
+        final InputEditor editor = doc.inputEditorFactory.createInputEditor(prior.distInput, prior, doc);
+        box.getChildren().add((Node)editor);
+        comboBox.getSelectionModel().select(availableBEASTObjects.get(0));
+        
+        comboBox.setOnAction(e -> {
+            @SuppressWarnings("unchecked")
+			ComboBox<BeautiSubTemplate> comboBox1 = (ComboBox<BeautiSubTemplate>) e.getSource();
+
+            BeautiSubTemplate template = (BeautiSubTemplate) comboBox1.getSelectionModel().getSelectedItem();
+            try {
+                template.createSubNet(new PartitionContext(), prior, prior.distInput, true);
+                final InputEditor editor2 = doc.inputEditorFactory.createInputEditor(prior.distInput, prior, doc);
+                box.getChildren().remove(1);
+                box.getChildren().add((Node) editor2);
+            } catch (Exception e1) {
+                e1.printStackTrace();
+            }
+        });
+        
+        primaryStage.setScene(new Scene(box, 1024, 650));
+        primaryStage.show();		
+	}
 
     public static void main(String[] args) {
-		try {
-			new DistributionViewer();
-		} catch (XMLParserException | SAXException | IOException | ParserConfigurationException e) {
-			e.printStackTrace();
-		}
+		launch(DistributionViewer.class, args);
 	}
 
 }
